78884e657de6933a9a808a62ce221a52501e63c1
Ronaldmolinares <108416072+Ronaldmolinares@users.noreply.github.com>
2025-06-20 18:32:35 -0500

    fix minor details

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
-const loinBtn = document.getElementById('login');
+const loginBtn = document.getElementById('login');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add('active');
 });
 
-loinBtn.addEventListener('click', () => {
+loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
 });
